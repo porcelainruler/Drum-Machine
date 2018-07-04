a845908f3db2b592e83aa583ed92103849d59397
Shubham <35026297+porcelainruler@users.noreply.github.com>
2018-07-04 21:02:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var Q=document.getElementById("Q");
+var W=document.getElementById("W");
+var E=document.getElementById("E");
+var A=document.getElementById("A");
+var S=document.getElementById("S");
+var D=document.getElementById("D");
+var Z=document.getElementById("Z");
+var X=document.getElementById("X");
+var C=document.getElementById("C");
+
+var display=document.getElementById("display");
